a07e83db5734d28b034fd7845e9bd17a3cd40341
あっつー！ <xx09fort05nitexx@gmail.com>
2026-06-26 23:34:42 +0900

    Update todo click event

---
+++ script.js
@@ -0,0 +1,7 @@
+const todoInput=document.getElementById("todoInput");
+const addBtn =document.getElementById("addBtn");
+const todoList = document.getElementById("todoList");
+
+addBtn.addEventListener("click",function(){
+  console.log("クリックされた");
+})
