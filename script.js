fff840412daa0cfb27c521836477a39622aa8896
xx09fort05nitexx-coder <xx09fort05nitexx@gmail.com>
2026-09-26 06:46:58 +0900

    Add todo button click event

--- script.js
+++ script.js
@@ -3,5 +3,11 @@ const addBtn =document.getElementById("addBtn");
 const todoList = document.getElementById("todoList");
 
 addBtn.addEventListener("click",function(){
-  console.log("クリックされた");
-})
+  const todoText = todoInput.value;
+  
+  const li =document.createElement("li");
+
+  li.textContent = todoText;
+
+  todoList.appendChild(li);
+});
